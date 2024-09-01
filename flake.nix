237781b5f6bc9607216d4b5b5e8b711035d7e7c1
brndnrphl <brndnrphl@gmail.com>
2024-09-01 20:32:27 +0700

    Changes to be committed: 	new file:   flake.nix

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "A simple NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    phps.url = "github:loophp/nix-shell";
+  };
+
+  pkgs = import inputs.nixpkgs {
+    inherit system;
+    overlays = [
+        inputs.phps.overlays.default
+    ];
+  };
+}
