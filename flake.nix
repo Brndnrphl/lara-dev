1ab4451dcb8bc47aa200cfedd114c939d8255156
brndnrphl <brndnrphl@gmail.com>
2024-09-02 20:12:14 +0700

    modified:   flake.nix

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
         pkgs = import nixpkgs {
           inherit system;
           overlays = [
-            phps.overlay
+            phps.overlays.default
           ];
         };
       });
