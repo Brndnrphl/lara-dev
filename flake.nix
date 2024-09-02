caa8848a5a276801ca10df3bcca369a21a141410
brndnrphl <brndnrphl@gmail.com>
2024-09-02 19:34:44 +0700

    modified:   flake.nix

--- flake.nix
+++ flake.nix
@@ -1,4 +1,5 @@
-{ description = "A simple NixOS flake";
+{
+  description = "A simple NixOS flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -8,27 +9,16 @@
 
   outputs = { self, nixpkgs, flake-utils, phps }:
     flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ phps.overlays.default ];
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ phps.overlays.default ];
+      };
+    in {
+      devShell = pkgs.mkShell {
+        default = pkgs.mkShell {
+          packages = with pkgs; [ nodejs ];
         };
-      in {
-        devShell = pkgs.mkShell {
-          buildInputs = [
-            pkgs.nodejs
-          ];
-        };
-
-        # Define the packages attribute
-        packages.x86_64-linux.default = pkgs.mkDerivation {
-          name = "my-package";
-          builder = builtins.toFile "builder.sh" ''
-            echo "Hello, World!"
-          '';
-          args = {
-            inherit system;
-          };
-        };
-      });
+      };
+    });
 }
